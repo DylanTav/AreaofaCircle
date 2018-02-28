b75f5173cf6245e8e42950e48fe1cf7972adf44d
dytav9540 <dytav9540@7KZL6Y1.Student.UGDSB.ED>
2018-02-28 11:28:16 -0500

    This is my first commit

---
+++ src/areaofthecircle/AreaoftheCircle.java
@@ -0,0 +1,23 @@
+
+/*
+This program was made by Dylan T
+On February 28th, 2018
+To display Wayne Gretzky and his number on a seperate line
+*/
+
+package areaofthecircle;
+
+
+public class AreaoftheCircle {
+    
+    public static void main(String[] args) {
+        
+        double Area; // Creates a varible double called Area
+        
+        Area = 3.14 * 225; //Area holds an equation 3.14 times 225
+        
+        System.out.println("The area of the circle is " + Area); // Print The area of the circle is and the anwser to Area 
+        
+    }
+    
+}
